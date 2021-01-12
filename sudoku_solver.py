af709e75024e61f5511051c956bd7fad343f8f50
Tim Tanida <37686657+ttanida@users.noreply.github.com>
2021-01-12 04:32:52 +0100

    Set MainWindow size to fixed

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(631, 646)
+        MainWindow.setFixedSize(631, 646)
         font = QtGui.QFont()
         font.setPointSize(24)
         MainWindow.setFont(font)
